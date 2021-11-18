862a6b9a78c1fa8b432d21d3713f812a22c56451
Steve Ryan <steveryan@github.com>
2021-11-18 18:17:25 -0500

    some updates to make things cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let mut arguments =std::env::args().skip(1);
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
-    let write_result =  write_database(key, value);
-    match write_result {
-        Ok(_) => println!("write success"),
-        Err(e) => println!("write error: {}", e),
-    }
+    write_database(key, value)
 }
 
 fn write_database(key: String, value: String) -> Result<(), std::io::Error> {
-    std::fs::write("kv.db", format!("{},{}", key, value))
+    std::fs::write("kv.db", format!("{}\t{}", key, value))
 }

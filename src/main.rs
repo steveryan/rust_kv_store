eb562839476f0fe40f045108d1f1323f6c6cdb7b
Steve Ryan <steveryan@github.com>
2021-11-19 13:54:57 -0500

    did some more work. Still need to implement the writing of the in memory hash map to the file

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ fn main() -> Result<(), std::io::Error> {
     let mut arguments =std::env::args().skip(1);
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
-    write_database(key, value)
+    let mut db = Database::from_disk()?;
+    db.insert(key, value);
+    db.flush()?;
+    drop(db);
+    Ok(())
 }
 
 struct Database {
@@ -12,13 +16,24 @@ struct Database {
 }
 
 impl Database {
-    fn from_disk() -> Database {
-        db = Database {
-            hashmap: HashMap::new(),
+    fn from_disk() -> Result<Database, std::io::Error> {
+        let mut hashmap = HashMap::new();
+        let contents = std::fs::read_to_string("kv.db")?;
+        for line in contents.lines() {
+            let mut chunks = line.split("\t");
+            let key = chunks.next().unwrap();
+            let value = chunks.next().unwrap();
+            hashmap.insert(key.to_string(), value.to_string());
         }
+        Ok(Database { hashmap })
     }
-}
 
-fn write_database(key: String, value: String) -> Result<(), std::io::Error> {
-    std::fs::write("kv.db", format!("{}\t{}", key, value))
+    fn insert(&mut self, key: String, value: String) {
+        self.hashmap.insert(key, value);
+    }
+
+    fn flush(&self) -> std::io::Result<()> {
+        let contents :String = todo!("implement this");
+        std::fs::write("kv.db", contents)
+    }
 }

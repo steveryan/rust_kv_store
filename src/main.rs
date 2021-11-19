ee9a20d70651a79070c6e4d4b864bba3f294dcf9
Steve Ryan <steveryan@github.com>
2021-11-18 19:35:34 -0500

    create db struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use std::collections::HashMap;
+
 fn main() -> Result<(), std::io::Error> {
     let mut arguments =std::env::args().skip(1);
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
     write_database(key, value)
+}
+
+struct Database {
+    hashmap: HashMap<String, String>,
+}
+
+impl Database {
+    fn from_disk() -> Database {
+        db = Database {
+            hashmap: HashMap::new(),
+        }
+    }
 }
 
 fn write_database(key: String, value: String) -> Result<(), std::io::Error> {
